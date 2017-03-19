5ada186b78919071ad7f7d738ca1ca9db2fcba7f
Zikang Zhao <z73zhao@uwaterloo.ca>
2017-03-19 05:42:09 -0400

    Init

--- FolderWatcher/FolderWatcher.cpp
+++ FolderWatcher/FolderWatcher.cpp
@@ -2,10 +2,203 @@
 //
 
 #include "stdafx.h"
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include "dirent.h"
+#include <string>
 
+void RefreshDirectory(LPTSTR);
+void RefreshTree(LPTSTR);
+void WatchDirectory(LPTSTR);
+void GetDirList(char* lpDir, int indent);
+void printByIndentLevel(char* str, int indent);
+char* concatPath(char* prefix, char* path);
 
-int main()
+using namespace std;
+
+void _tmain(int argc, TCHAR *argv[])
 {
-    return 0;
+	if (argc != 2)
+	{
+		_tprintf(TEXT("Usage: %s <dir>\n"), argv[0]);
+		return;
+	}
+
+	WatchDirectory(argv[1]);
 }
 
+void WatchDirectory(LPTSTR lpDir)
+{
+	DWORD dwWaitStatus;
+	HANDLE dwChangeHandles[3];
+
+	// Watch the directory for file creation and deletion. 
+
+	dwChangeHandles[0] = FindFirstChangeNotification(
+		lpDir,                         // directory to watch 
+		TRUE,                         // do not watch subtree 
+		FILE_NOTIFY_CHANGE_FILE_NAME); // watch file name changes 
+
+	if (dwChangeHandles[0] == INVALID_HANDLE_VALUE)
+	{
+		printf("\n ERROR: FindFirstChangeNotification function failed.\n");
+		ExitProcess(GetLastError());
+	}
+
+	// Watch the subtree for directory creation and deletion. 
+
+	dwChangeHandles[1] = FindFirstChangeNotification(
+		lpDir,                       // directory to watch 
+		TRUE,                          // watch the subtree 
+		FILE_NOTIFY_CHANGE_DIR_NAME);  // watch dir name changes 
+
+	if (dwChangeHandles[1] == INVALID_HANDLE_VALUE)
+	{
+		printf("\n ERROR: FindFirstChangeNotification function failed.\n");
+		ExitProcess(GetLastError());
+	}
+
+	// Watch the subtree for directory creation and deletion. 
+
+	dwChangeHandles[2] = FindFirstChangeNotification(
+		lpDir,                       // directory to watch 
+		TRUE,                          // watch the subtree 
+		FILE_NOTIFY_CHANGE_SIZE);  // watch dir name changes 
+
+	if (dwChangeHandles[2] == INVALID_HANDLE_VALUE)
+	{
+		printf("\n ERROR: FindFirstChangeNotification function failed.\n");
+		ExitProcess(GetLastError());
+	}
+
+	// Make a final validation check on our handles.
+
+	if ((dwChangeHandles[0] == NULL) || (dwChangeHandles[1] == NULL) || (dwChangeHandles[2] == NULL))
+	{
+		printf("\n ERROR: Unexpected NULL from FindFirstChangeNotification.\n");
+		ExitProcess(GetLastError());
+	}
+
+	// Change notification is set. Now wait on both notification 
+	// handles and refresh accordingly. 
+
+	_tprintf(TEXT("Waiting for change notification on %s...\n"), lpDir);
+
+	while (TRUE)
+	{
+		printf("Current Directory:\n");
+		GetDirList((char*)lpDir, 0);
+		printf("------------------------------------------------\n");
+
+		// Wait for notification.
+		dwWaitStatus = WaitForMultipleObjects(3, dwChangeHandles, FALSE, INFINITE);
+
+		switch (dwWaitStatus)
+		{
+		case WAIT_OBJECT_0:
+
+			// A file was created, renamed, or deleted in the directory.
+			// Refresh this directory and restart the notification.
+
+			RefreshDirectory(lpDir);
+			if (FindNextChangeNotification(dwChangeHandles[0]) == FALSE)
+			{
+				printf("\n ERROR: FindNextChangeNotification function failed.\n");
+				ExitProcess(GetLastError());
+			}
+			break;
+
+		case WAIT_OBJECT_0 + 1:
+
+			// A directory was created, renamed, or deleted.
+			// Refresh the tree and restart the notification.
+
+			RefreshTree(lpDir);
+			if (FindNextChangeNotification(dwChangeHandles[1]) == FALSE)
+			{
+				printf("\n ERROR: FindNextChangeNotification function failed.\n");
+				ExitProcess(GetLastError());
+			}
+			break;
+
+		case WAIT_OBJECT_0 + 2:
+
+			// A file was modified.
+			// Refresh the tree and restart the notification.
+
+			RefreshTree(lpDir);
+			if (FindNextChangeNotification(dwChangeHandles[2]) == FALSE)
+			{
+				printf("\n ERROR: FindNextChangeNotification function failed.\n");
+				ExitProcess(GetLastError());
+			}
+			break;
+
+		case WAIT_TIMEOUT:
+
+			// A timeout occurred, this would happen if some value other 
+			// than INFINITE is used in the Wait call and no changes occur.
+			// In a single-threaded environment you might not want an
+			// INFINITE wait.
+
+			printf("\nNo changes in the timeout period.\n");
+			break;
+
+		default:
+			printf("\n ERROR: Unhandled dwWaitStatus.\n");
+			ExitProcess(GetLastError());
+			break;
+		}
+	}
+}
+
+void RefreshDirectory(LPTSTR lpDir)
+{
+	_tprintf(TEXT("Directory (%s) changed.\n"), lpDir);
+}
+
+void RefreshTree(LPTSTR lpDir)
+{
+	_tprintf(TEXT("Directory tree (%s) changed.\n"), lpDir);
+}
+
+void GetDirList(char* lpDir, int indent)
+{
+	DIR *dir;
+	struct dirent *ent;
+	if ((dir = opendir(lpDir)) != NULL) {
+		/* print all the files and directories within directory */
+		while ((ent = readdir(dir)) != NULL) {
+			if (ent->d_type == DT_DIR && strcmp(ent->d_name, ".") != 0 && strcmp(ent->d_name, "..") != 0) {
+				printByIndentLevel(ent->d_name, indent);
+				char* newName = concatPath(lpDir, ent->d_name);
+				GetDirList(newName, indent + 2);
+				free(newName);
+			}
+			else if (ent->d_type == DT_REG) {
+				printByIndentLevel(ent->d_name, indent);
+			}
+		}
+		closedir(dir);
+	}
+	else {
+		/* could not open directory */
+		perror("");
+	}
+}
+
+void printByIndentLevel(char* str, int indent)
+{
+	for (int i = 0; i < indent; i++) printf(" ");
+	printf("%s\n", str);
+}
+
+char* concatPath(char* prefix, char* path)
+{
+	char* newName = (char*)malloc(512 * sizeof(char));
+	strcpy(newName, prefix);
+	strcat(newName, path);
+	strcat(newName, "\\");
+	return newName;
+}
